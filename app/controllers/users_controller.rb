d7adada2b29a3373e0d77daa712e19f647552d28
saito743 <yuka.saito21@gmail.com>
2020-09-16 11:57:22 +0000

    [Update]bootstrap導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
@@ -34,4 +35,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+
 end
