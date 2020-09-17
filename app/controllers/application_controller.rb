3ba076cf6e16a2e695a7fc080e9c515a9fd0ab38
saito743 <yuka.saito21@gmail.com>
2020-09-17 11:00:01 +0000

    [Fix]login済ユーザーのsign_in遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for(resource)
-    user_path(@user)
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
